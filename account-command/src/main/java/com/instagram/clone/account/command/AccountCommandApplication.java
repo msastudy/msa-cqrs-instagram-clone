69e6745bf2a26bb48a137abf4b059d9c54eb3805
kyung <leeseok8347@gmail.com>
2020-03-08 19:19:57 +0900

    init project

---
+++ account-command/src/main/java/com/instagram/clone/account/command/AccountCommandApplication.java
@@ -0,0 +1,12 @@
+package com.instagram.clone.account.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountCommandApplication.class, args);
+    }
+}
